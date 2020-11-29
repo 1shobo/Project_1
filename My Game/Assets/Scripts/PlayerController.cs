8aa4aac3d5858905b2e73dec3223ccd093a93521
1shobo <b00125923@mytudublin.ie>
2020-11-29 15:48:19 +0000

    Unity Project added

---
+++ My Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float zRange = 17;
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public GameObject projectilePrefab;
+    public bool gameOver = false;
+
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+           
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+    }
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Alien"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+    }
+
+}
---
+++ My Game/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    public GameObject[] alienPrefabs;
+    private float spawnRangeX = 80;
+    private float spawnRangeZ = 20;
+    private float startDelay = 2;
+    private float startDelayA = 1;
+    private float spawnInterval = 1.5f;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomObstacle", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAlien", startDelayA, spawnInterval);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void SpawnRandomObstacle() {
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefabs[obstacleIndex], new Vector3(-spawnRangeX, 0, Random.Range(-spawnRangeZ, spawnRangeZ)),
+           obstaclePrefabs[obstacleIndex].transform.rotation);
+        }
+       
+
+    }
+
+    void SpawnRandomAlien()
+    {
+        int alienIndex = Random.Range(0, alienPrefabs.Length);
+
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(alienPrefabs[alienIndex], new Vector3(-spawnRangeX, 4, Random.Range(-spawnRangeZ, spawnRangeZ)),
+            alienPrefabs[alienIndex].transform.rotation);
+        }
+
+    }
+}
